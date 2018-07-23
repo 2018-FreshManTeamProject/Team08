4f4da800026e2bf5a19f816c8daf078d2f9679f9
AL-XQ <thealienxq@gmail.com>
2018-07-23 11:45:04 +0900

    クレジットを勝手に変更されないように調整

--- Team08/Scene/Title/UI/Readme.cs
+++ Team08/Scene/Title/UI/Readme.cs
@@ -32,7 +32,27 @@ namespace MouseTrash.Scene.Title.UI
             Text = GetText("ReadmeText");
             message = new Label(graphicsDevice, this);
             message.TextSize = 24f;
-            message.Text = GetText("Readme");
+            #region クレジット
+            message.Text = "「MouseTrash」は「InfinityGame」サブエンジンを使用\r\n" +
+                           "\r\n" +
+                           "「MouseTrash」について：\r\n" +
+                           "ゲーム起案：爪橋（暫定名字表示）\r\n" +
+                           "ゲーム検討：爪橋（暫定名字表示）\r\n" +
+                           "　　　　　　吉村（暫定名字表示）\r\n" +
+                           "　　　　　　XIE SHAOJIE\r\n" +
+                           "プログラム著作者：XIE SHAOJIE\r\n" +
+                           "グラフィック著作者：吉村（主人公関係、暫定名字表示）\r\n" +
+                           "                    爪橋（マウス関係、暫定名字表示）\r\n" +
+                           "                    XIE SHAOJIE（UI関係）\r\n" +
+                           "\r\n" +
+                           "「InfinityGame」について：\r\n" +
+                           "バージョン：1.0.1.4\r\n" +
+                           "バージョン作成年月日：2018-7-22\r\n" +
+                           "著作者：XIE SHAOJIE\r\n" +
+                           "\r\n" +
+                           "もし「MouseTrash」と「InfinityGame」について疑問などがあったら、\r\n" +
+                           "本人たちに連絡してください。";
+            #endregion クレジット
             message.Location = ((Size - message.Size) / 2).ToPoint();
             base.PreLoadContent();
         }
